fa02d84d57406a014fd588d815ee1dcf8ae78b63
韩洁 <h1603459540@163.com>
2020-03-12 15:38:31 +0800

    commit test code and part function code

--- Poker.py
+++ Poker.py
@@ -1,2 +1,115 @@
-def Poker(poker_hands):
+white_win = "White wins"
+black_win = "Black wins"
+tie = "Tie"
+
+#Input a pair of poker hands and return who wins
+#poker_hands format:"Black:2H 3D 5S 9C KD;White:2C 3H 4S 8C AH"
+def Poker(pair_poker_hands):
+
+    black_poker_hands, white_poker_hands = pair_poker_hands.split(';')
+    black_poker_hands = black_poker_hands.split(':')[1].split(" ")
+    white_poker_hands = white_poker_hands.split(':')[1].split(" ")
+    black_kind_rank = get_kind_rank(black_poker_hands)
+    white_kind_rank = get_kind_rank(white_poker_hands)
+    if black_kind_rank < white_kind_rank:
+        return white_win
+    elif black_kind_rank > white_kind_rank:
+        return black_win
+    else:
+        black_number = get_number(black_poker_hands)
+        white_number = get_number(white_poker_hands)
+        if black_kind_rank == 1:
+            win = high_card(black_number,white_number)
+            return win
+        elif black_kind_rank == 2:
+            win = pair(black_number,white_number)
+            return win
+        elif black_kind_rank == 3:
+            win = two_pairs(black_number,white_number)
+            return win
+        elif black_kind_rank == 4:
+            win = three_of_a_kind(black_number,white_number)
+            return win
+        elif black_kind_rank == 5:
+            win = straight(black_number,white_number)
+            return win
+        elif black_kind_rank == 6:
+            win = flush(black_number, white_number)
+            return win
+        elif black_kind_rank == 7:
+            win = full_house(black_number, white_number)
+            return win
+        elif black_kind_rank == 8:
+            win = four_of_a_kind(black_number, white_number)
+            return win
+        else:
+            win = straight_flush(black_number, white_number)
+            return win
+
+def high_card(black_number, white_number):
     return 0
+
+def pair(black_number,white_number):
+   return 0
+def two_pairs(black_number, white_number):
+    return 0
+def three_of_a_kind(black_number, white_number):
+    return 0
+def straight(black_number, white_number):
+    return 0
+def flush(black_number, white_number):
+    return 0
+def full_house(black_number, white_number):
+    return 0
+def four_of_a_kind(black_number, white_number):
+    return 0
+def straight_flush(black_number, white_number):
+    return 0
+
+def get_key(dict, value):
+    return [k for (k, v) in dict.items() if v == value]
+
+def is_same_suit(poker_hands):
+    suit = [item[1] for item in poker_hands]
+    if suit[0] == suit[1] == suit[2] == suit[3] == suit[4]:
+        return True
+    return False
+
+def get_number(poker_hands):
+    re = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    number = [re[item[0]] if item[0] in re else int(item[0]) for item in poker_hands]
+    number.sort()
+    return number
+
+def get_same_number(number):
+    dict = {}
+    for num in number:
+        if num in dict.keys():
+            dict[num] += 1
+        else:
+            dict[num] = 1
+    return dict
+
+def get_kind_rank(poker_hands):
+    number = get_number(poker_hands)
+    #print(number)
+    if is_same_suit(poker_hands):
+        if number[0]+1 == number[1] and number[1]+1 == number[2] and number[2]+1 == number[3] and number[3]+1 == number[4]:
+            return 9
+        return 6
+    if number[0] + 1 == number[1] and number[1] + 1 == number[2] and number[2] + 1 == number[3] and number[3] + 1 == number[4]:
+        return 5
+    same_number_dict = get_same_number(number)
+    max_same_num = max(same_number_dict.values())
+    min_same_num = min(same_number_dict.values())
+    if max_same_num == 4:
+        return 8
+    if max_same_num == 3:
+        if min_same_num == 2:
+            return 7
+        return 4
+    if max_same_num == 2:
+        if len(same_number_dict) == 3:
+            return 3
+        return 2
+    return 1
--- Poker_test.py
+++ Poker_test.py
@@ -1,21 +1,66 @@
 import unittest
 
-import Poker
-
+from Poker import get_same_number,Poker,get_kind_rank
 
 class TestPoker(unittest.TestCase):
-    def test_1(self):
+
+    def test_kind_1(self):
+        assert get_kind_rank(['2H','3D','5S','9C','KD']) == 1
+
+    def test_kind_2(self):
+        assert get_kind_rank(['2H','2D','5S','9C','KD']) == 2
+
+    def test_kind_3(self):
+        assert get_kind_rank(['2H','2D','5S','5C','KD']) == 3
+
+    def test_kind_4(self):
+        assert get_kind_rank(['2H','2D','2S','9C','KD']) == 4
+
+    def test_kind_5(self):
+        assert get_kind_rank(['2H','3D','5S','4C','6D']) == 5
+
+    def test_kind_6(self):
+        assert get_kind_rank(['2S','8S','AS','QS','3S']) == 6
+
+    def test_kind_7(self):
+        assert get_kind_rank(['2H','2D','2S','9C','9D']) == 7
+
+    def test_kind_8(self):
+        assert get_kind_rank(['2H','2D','2S','2C','KD']) == 8
+
+    def test_kind_9(self):
+        assert get_kind_rank(['2H','3H','5H','6H','4H']) == 9
+
+
+    def test_high_card(self):
         assert Poker("Black:2H 3D 5S 9C KD;White:2C 3H 4S 8C AH") == "White wins"
 
-    def test_2(self):
+    def test_two_kind(self):
         assert Poker("Black:2H 4S 4C 2D 4H;White:2S 8S AS QS 3S") == "Black wins"
 
-    def test_3(self):
-        assert Poker("Black:2H 3D 5S 9C KD;White:2C 3H 4S 8C KH") == "Black wins"
+    def test_pair(self):
+        assert Poker("Black:2H 2D 5S 9C KD;White:2C 2S 4S KC 8H") == "Black wins"
 
-    def test_4(self):
-        assert Poker("Black:2H 3D 5S 9C KD;White:2D 3H 5C 9S KH") == "Tie"
+    def test_two_pairs(self):
+        assert Poker("Black:2H 2D 4S 4C KD;White:2S 2C 4D 4H KH") == "Tie"
 
+    def test_three_of_a_kind(self):
+        assert Poker("Black:2H 2D 3S 2C KD;White:2S 4H 4C 4S KH") == "White wins"
+
+    def test_straight(self):
+        assert Poker("Black:2H 3D 4S 5C 6D;White:TD JH QC KS AH") == "White wins"
+
+    def test_flush(self):
+        assert Poker("Black:2H 5H 7H 9H QH;White:3H 8H QH KH AH") == "White wins"
+
+    def test_full_hose(self):
+        assert Poker("Black:2H KD KS 2S KH;White:3D 8H 8C 8S 3H") == "Black wins"
+
+    def test_four_of_a_kind(self):
+        assert Poker("Black:6H 6D 3S 6S 6C;White:8D 8H 8C 8S 3H") == "White wins"
+
+    def test_straight_flush(self):
+        assert Poker("Black:5H 6H 7H 8H 9H;White:3D 4D 5D 6D 7D") == "Black wins"
 
 if __name__ == "__main__":
     unittest.main()
